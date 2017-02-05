1e2a4b4d4464a8ec73d0c5ddd074a1f949f0da23
Sean Collings <seanrcollings@gmail.com>
2017-02-04 17:58:53 -0700

    Algorithm complete

--- spreadsheets_to_yaml_algorithm.py
+++ spreadsheets_to_yaml_algorithm.py
@@ -5,18 +5,17 @@ input_file = open("armor_spreadsheets(DS2).csv", "r")
 output_file = open("Armor(DS2).yaml", "w")
 
 lines = input_file.readlines()
-keys = (lines[0].split("|"))
+keys = lines[0].rstrip().split("|")
 equipment = []
 
 
 for line in lines[1:]:
-	line = line.split("|")
-	test_dict = {}
+	line = line.rstrip().split("|")
+	line_dict = {}
 	print(line)
 	for index, val in enumerate(line):
-		test_dict[keys[index]] = val
-	equipment.append(test_dict)
-
+		line_dict[keys[index]] = val
+	equipment.append(line_dict)
 
 
 output_file.write(yaml.dump(equipment, default_flow_style = False))
